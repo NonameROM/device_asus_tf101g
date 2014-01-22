cf982c83321dcbed88a49f29eeaef95e90ecbc7c
ShevT <shev_t@mail.ru>
2014-01-22 08:47:07 +0400

    BoardConfig.mk: rild sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,8 +110,8 @@ BOARD_SEPOLICY_UNION := \
     file_contexts \
     file.te \
     device.te \
-    domain.te
-#    rild.te
+    domain.te \
+    rild.te
 
 # recovery
 RECOVERY_FSTAB_VERSION := 2
